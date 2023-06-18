4221033cd6685581283bbf7935078836bd6dd827
Beezyz <116973964+Beezyz@users.noreply.github.com>
2023-06-18 15:12:52 -0300

    updated models files and added eslint

---
+++ models/User.js
@@ -0,0 +1,48 @@
+// Importing the Mongoose library
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+// defining the schema
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/, 'Please enter a valid email address']
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought'
+      }
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+      }
+    ]
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    id: false
+  }
+);
+// Defines virtual property friendCount, defines getter function calculating length of friends array
+userSchema.virtual('friendCount').get(function () {
+  return this.friends.length;
+});
+
+const User = mongoose.model('User', userSchema);
+// exporting to make available for other parts of the application
+module.exports = User;
