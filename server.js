c6201fc4b400c5becfe68ee68c68b7bb019fafd2
Beezyz <116973964+Beezyz@users.noreply.github.com>
2023-06-18 13:07:45 -0300

    set up inital server.js with mongodb

---
+++ server.js
@@ -0,0 +1,25 @@
+// server.js
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Middleware
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost/social_network_db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
+
+// Routes
+
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
